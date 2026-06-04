a495be1d9cc62d76114f47b2f5131c82e7833ea6
Shhhhiiple <justinjhuwen@gmail.com>
2026-06-04 14:10:43 +0800

    update
    Removed sensitive Firebase configuration details.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,13 +9,13 @@ import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCug_ctFs7fqrcyEtcHz_h763CVX3dyfj4",
-  authDomain: "fitchoice-64b19.firebaseapp.com",
-  projectId: "fitchoice-64b19",
-  storageBucket: "fitchoice-64b19.firebasestorage.app",
-  messagingSenderId: "734471567720",
-  appId: "1:734471567720:web:233e9376b8c0f1b037bedd",
-  measurementId: "G-X5DLJBP5B6",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
